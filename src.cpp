bbd419b2a8b67122ecc0720e9c764d8b4379ada9
nazoSAUNA <99536641+nazonoSAUNA@users.noreply.github.com>
2025-01-18 23:18:11 +0900

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -1,16 +1,15 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <aviutl.hpp>
 #include <exedit.hpp>
 
 
-ExEdit::Filter ef;
 inline static char name[] = "音声クロスフェード";
 
 inline static short* (__cdecl* create_audio_cache)(ExEdit::ObjectFilterIndex ofi, int size, int func_idx, int fillzero);
 inline static BOOL(__cdecl* exedit_audio_func_main)(AviUtl::FilterPlugin* fp, AviUtl::FilterProcInfo* fpip, int end_layer, int add_frame, int audio_speed, int milliframe, int scene_idx, ExEdit::ObjectFilterIndex ofi);
 
-AviUtl::FilterPlugin* exeditfp;
-AviUtl::FilterPlugin* get_exeditfp(ExEdit::Filter* efp) {
+AviUtl::FilterPlugin* exedit_audio_fp;
+AviUtl::FilterPlugin* get_exedit_audio_fp(ExEdit::Filter* efp) {
     AviUtl::SysInfo si;
     efp->aviutl_exfunc->get_sys_info(NULL, &si);
 
@@ -23,26 +22,28 @@ AviUtl::FilterPlugin* get_exeditfp(ExEdit::Filter* efp) {
     return NULL;
 }
 BOOL func_init(ExEdit::Filter* efp) {
-    exeditfp = get_exeditfp(efp);
-    if (exeditfp != NULL) {
-        create_audio_cache = reinterpret_cast<decltype(create_audio_cache)>((int)exeditfp->dll_hinst + 0x2a8f0);
-        exedit_audio_func_main = reinterpret_cast<decltype(exedit_audio_func_main)>((int)exeditfp->dll_hinst + 0x49ca0);
+    exedit_audio_fp = get_exedit_audio_fp(efp);
+    if (exedit_audio_fp != NULL) {
+        create_audio_cache = reinterpret_cast<decltype(create_audio_cache)>((int)exedit_audio_fp->dll_hinst + 0x2a8f0);
+        exedit_audio_func_main = reinterpret_cast<decltype(exedit_audio_func_main)>((int)exedit_audio_fp->dll_hinst + 0x49ca0);
     }
     return TRUE;
 }
 
 BOOL func_proc(ExEdit::Filter* efp, ExEdit::FilterProcInfo* efpip) {
+    if (exedit_audio_fp == NULL) return TRUE;
+
     int sc_frame = max(0, efp->frame_start_chain - 1);
 
     AviUtl::FilterProcInfo fpi = *(AviUtl::FilterProcInfo*)efpip;
     fpi.flag = (AviUtl::FilterProcInfo::Flag)ExEdit::FilterProcInfo::Flag::nesting;
     fpi.frame = sc_frame;
-    short* sub = create_audio_cache(efp->processing, efpip->audio_ch * efpip->audio_n * 2, efpip->v_func_idx + 1, 1);
+    short* sub = create_audio_cache(efp->processing, efpip->audio_ch * efpip->audio_n * sizeof(short), efpip->v_func_idx + 1, 1);
 
     if (sub == nullptr) {
         return FALSE;
     }
-    efp->aviutl_exfunc->get_audio_filtering(exeditfp, *reinterpret_cast<AviUtl::EditHandle**>((int)exeditfp->dll_hinst + 0x1a532c), efpip->frame_num, sub);
+    efp->aviutl_exfunc->get_audio_filtering(exedit_audio_fp, *reinterpret_cast<AviUtl::EditHandle**>((int)exedit_audio_fp->dll_hinst + 0x1a532c), efpip->frame_num, sub);
     fpi.audiop = sub;
     int audio_speed = efpip->audio_speed;
     if (audio_speed == 0) {
@@ -52,28 +53,35 @@ BOOL func_proc(ExEdit::Filter* efp, ExEdit::FilterProcInfo* efpip) {
         return FALSE;
     }
 
-    int rate = 32768 - (int)round((double)(efpip->frame + 1) * 32768.0 / (double)(efp->frame_end_chain - efp->frame_start_chain + 2));
+    double step_d = 1073741824.0 / (double)(efp->frame_end_chain - efp->frame_start_chain + 1);
+    int step = (int)round(step_d * (double)audio_speed * 0.000001 / (double)efpip->audio_n);
+    int rate = 1073741824 - (int)round((double)efpip->frame * step_d);
+    short* rate_hi = (short*)&rate + 1;
 
     short* main = efpip->audio_p;
-    for (int i = efpip->audio_ch * efpip->audio_n; 0 < i; i--) {
-        *main += (*sub - *main) * rate >> 15;
-        main++; sub++;
+    for (int i = efpip->audio_n; 0 < i; i--) {
+        for (int ch = efpip->audio_ch; 0 < ch; ch--) {
+            *main += (*sub - *main) * *rate_hi >> 14;
+            main++; sub++;
+        }
+        rate -= step;
     }
     return TRUE;
 }
 
+ExEdit::Filter ef = {
+    .flag = ExEdit::Filter::Flag::Audio,
+    .name = name,
+    .track_n = 0,
+    .check_n = 0,
+    .func_proc = func_proc,
+    .func_init = func_init,
+};
 ExEdit::Filter* filter_list[] = {
     &ef,
     NULL
 };
 
 EXTERN_C __declspec(dllexport)ExEdit::Filter** __stdcall GetFilterTableList() {
-    ef.flag = ExEdit::Filter::Flag::Audio;
-    ef.name = name;
-    ef.track_n = 0;
-    ef.check_n = 0;
-    ef.func_proc = (func_proc);
-    ef.func_init = (func_init);
-
     return filter_list;
 }
